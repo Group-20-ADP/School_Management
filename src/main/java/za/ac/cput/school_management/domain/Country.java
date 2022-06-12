497152297ed2fcce84c1ae7213b3dd37335f4b5d
AmyrHumaamMohamed <amyrhumaam@gmail.com>
2022-06-12 15:48:57 +0200

    Country Entity with Tests Country Factory with Tests

--- src/main/java/za/ac/cput/school_management/domain/Country.java
+++ src/main/java/za/ac/cput/school_management/domain/Country.java
@@ -1,4 +1,64 @@
 package za.ac.cput.school_management.domain;
-
+/**
+ *
+ * Humaam Mohamed
+ * 214044750
+ * Country Entity
+ *
+ */
 public class Country {
+
+    private String id;
+    private String name;
+
+    private Country (CountryBuilder builder){
+
+        this.id=builder.id;
+        this.name=builder.name;
+
+    }
+
+    public String setId(String id) {
+        this.id = id;
+        return id;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+   public static class CountryBuilder{
+
+        private String id;
+        private String name;
+
+       public CountryBuilder setId(String id) {
+           this.id = id;
+           return this;
+       }
+
+       public CountryBuilder setName(String name) {
+           this.name = name;
+           return this;
+       }
+
+
+       public Country builder(){
+
+           return new Country(this);
+       }
+   }
+
+
+
+
 }
