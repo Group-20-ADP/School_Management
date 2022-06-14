8cc06f857e56a07a100cf227549be55d559fa1d2
M.Nur <nurduncan12345@gmail.com>
2022-06-15 00:47:25 +0200

    Added City Factory Test

---
+++ src/test/java/za/ac/cput/school_management/factory/CityFactoryTest.java
@@ -0,0 +1,34 @@
+package za.ac.cput.school_management.factory;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.school_management.domain.City;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+
+    void createCity() {
+
+        var city = CityFactory.createCity("2587946", "Cape Town",);
+        assertNotNull (city);
+        System.out.println(city);
+    }
+
+
+}
+
