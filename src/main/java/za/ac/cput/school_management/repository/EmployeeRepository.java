3a5cc15551c8abcfb055bbda68561a016d94cb8c
Kurtney21 <kurtjantjies18@gmail.com>
2022-06-14 17:08:25 +0200

    Added q4 Services for employee: refer to Brief Q4

--- src/main/java/za/ac/cput/school_management/repository/EmployeeRepository.java
+++ src/main/java/za/ac/cput/school_management/repository/EmployeeRepository.java
@@ -1,9 +1,15 @@
 package za.ac.cput.school_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.school_management.domain.Employee;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
+    @Query("select u from Employee as u where u.email = :email")
+    Optional<Employee> findByEmail(@Param("email") String email);
 }
