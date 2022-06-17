4aa2392a4e852141c19934ffce60ab9d7f645644
athinikau <athinimbiko5@gmail.com>
2022-06-17 23:46:50 +0100

    "finished Student factory tester"

---
+++ src/test/java/za/ac/cput/school_management/factory/StudentFactoryTest.java
@@ -0,0 +1,25 @@
+package za.ac.cput.school_management.factory;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.school_management.domain.Student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentFactoryTest {
+    @Test
+    void createStudentWithInvalidEmail() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Student student = StudentFactory.createStudent("12345", "somenonsense",
+                    "Rob", "da", "banks");
+        });
+        System.out.println("exception = " + exception.getMessage());
+    }
+
+    @Test
+    void createCorrectStudent() {
+        Student student = StudentFactory.createStudent("12345", "test@bob.com",
+                "Rob", "da", "banks");
+        assertNotNull(student);
+        assertEquals("12345", student.getStudentId());
+        System.out.println("student with id " + student.getStudentId() + " has been created");
+    }
+}
