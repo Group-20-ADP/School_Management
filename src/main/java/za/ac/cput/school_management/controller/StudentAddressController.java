50009d3272befd4cae154ac0bbeaa7596b7c1055
Kurtney21 <kurtjantjies18@gmail.com>
2022-06-18 20:02:21 +0200

    Imports fixed

--- src/main/java/za/ac/cput/school_management/controller/StudentAddressController.java
+++ src/main/java/za/ac/cput/school_management/controller/StudentAddressController.java
@@ -9,6 +9,8 @@ import za.ac.cput.school_management.api.StudentAddressAPI;
 import za.ac.cput.school_management.domain.StudentAddress;
 import za.ac.cput.school_management.service.StudentAddress.IStudentAddressService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/student-address")
 public class StudentAddressController {
