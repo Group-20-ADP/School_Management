5f7d8e9ed68a64d6bfebed0b791159eb01e38816
Kurtney21 <kurtjantjies18@gmail.com>
2022-06-18 19:50:52 +0200

    Final Words

--- src/test/java/za/ac/cput/school_management/service/Employee/Impl/EmployeeServiceTest.java
+++ src/test/java/za/ac/cput/school_management/service/Employee/Impl/EmployeeServiceTest.java
@@ -1,20 +1,28 @@
 package za.ac.cput.school_management.service.Employee.Impl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import za.ac.cput.school_management.domain.Employee;
 import za.ac.cput.school_management.domain.valueobjects.Name;
 import za.ac.cput.school_management.factory.EmployeeFactory;
 import za.ac.cput.school_management.factory.NameFactory;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeServiceTest {
     Name name = NameFactory.createName("Rob", "da", "banks");
     private final Employee employee = EmployeeFactory.createEmployee("1","kurtjan@gmail.com",name);
 
 
+    private final EmployeeService service;
+
+    @Autowired
+    EmployeeServiceTest(EmployeeService service) {
+        this.service = service;
+    }
+
     @Test
     void create() {
+        service.create(employee);
+        System.out.println("Employee Created");
     }
 
     @Test
